4158cec7c765c8432b66cc9e0f85ab396c914830
mozart <mozart@titanium>
2023-04-13 06:20:11 +0000

    added vgg16 model

--- libs/model_archs.py
+++ libs/model_archs.py
@@ -2,6 +2,242 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import models, layers
 
+def vgg16D(input_shape, nb_classes):
+    # https://www.geeksforgeeks.org/cifar-10-image-classification-in-tensorflow/
+    return models.Sequential(
+        [
+            # 1st Conv Block
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=input_shape),
+            layers.BatchNormalization(),
+            layers.Dropout(0.3),
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 2nd Conv Block
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 3rd Conv Block
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 4th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 5th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.4),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.4),
+            
+            # Dense Layers
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(units=512, activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dense(units=nb_classes, activation='softmax')
+        ],
+        name='vgg16D',
+    )
+
+
+def vgg16C(input_shape, nb_classes):
+    # https://www.geeksforgeeks.org/cifar-10-image-classification-in-tensorflow/
+    return models.Sequential(
+        [
+            # 1st Conv Block
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=input_shape),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.2),
+            
+            # 2nd Conv Block
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.3),
+            
+            # 3rd Conv Block
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.4),
+            
+            # 4th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.4),
+            
+            # 5th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            layers.Dropout(0.4),
+            
+            # Dense Layers
+            layers.Flatten(),
+            layers.Dropout(0.5),
+            layers.Dense(units=1024, activation='relu'),
+            layers.BatchNormalization(),
+            layers.Dropout(0.5),
+            layers.Dense(units=1024, activation='relu'),
+            layers.Dropout(0.5),
+            layers.Dense(units=nb_classes, activation='softmax')
+        ],
+        name='vgg16C',
+    )
+
+
+def vgg16B(input_shape, nb_classes):
+    # https://www.geeksforgeeks.org/cifar-10-image-classification-in-tensorflow/
+    return models.Sequential(
+        [
+            # 1st Conv Block
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu', input_shape=input_shape),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 2nd Conv Block
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 3rd Conv Block
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=256, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 4th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 5th Conv Block
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # Dense Layers
+            layers.Flatten(),
+            layers.Dropout(0.2),
+            layers.Dense(units=1024, activation='relu'),
+            layers.Dropout(0.4),
+            layers.Dense(units=1024, activation='relu'),
+            layers.Dropout(0.4),
+            layers.Dense(units=nb_classes, activation='softmax')
+        ],
+        name='vgg16B',
+    )
+
+
+def vgg16A(input_shape, nb_classes):
+    # https://www.geeksforgeeks.org/cifar-10-image-classification-in-tensorflow/
+    return models.Sequential(
+        [
+            # 1st Conv Block
+            layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu', input_shape=input_shape),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=32, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 2nd Conv Block
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=64, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # 3rd Conv Block
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.Conv2D(filters=128, kernel_size=3, padding='same', activation='relu'),
+            layers.BatchNormalization(),
+            layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # # 4th Conv Block
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            # # 5th Conv Block
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.Conv2D(filters=512, kernel_size=3, padding='same', activation='relu'),
+            # layers.MaxPool2D(pool_size=2, strides=2, padding='same'),
+            
+            # Dense Layers
+            layers.Flatten(),
+            layers.Dropout(0.2),
+            layers.Dense(units=1024, activation='relu'),
+            layers.Dropout(0.2),
+            # layers.Dense(units=4096, activation='relu'),
+            layers.Dense(units=nb_classes, activation='softmax')
+        ],
+        name='vgg16A',
+    )
+
+
+
 def lenetA(input_shape, nb_classes):
     return models.Sequential(
         [
--- libs/utils.py
+++ libs/utils.py
@@ -4,6 +4,32 @@ from tensorflow import keras
 from .mixed_precision import SAFE_FLOAT_RANGE
 
 
+def prepare_cifar10():
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    x_train = x_train.astype(np.float32) / 255.0
+    x_test = x_test.astype(np.float32) / 255.0
+
+    n_classes = len(np.unique(y_train))
+    y_train = keras.utils.to_categorical(y_train, num_classes=n_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes=n_classes)
+    
+    # data augmentation
+    datagen = keras.preprocessing.image.ImageDataGenerator(
+    featurewise_center=False,  # set input mean to 0 over the dataset
+    samplewise_center=False,  # set each sample mean to 0
+    featurewise_std_normalization=False,  # divide inputs by std of the dataset
+    samplewise_std_normalization=False,  # divide each input by its std
+    zca_whitening=False,  # apply ZCA whitening
+    rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
+    width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+    height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+    horizontal_flip=True,  # randomly flip images
+    vertical_flip=False)  # randomly flip images
+    # (std, mean, and principal components if ZCA whitening is applied).
+    datagen.fit(x_train)
+    
+    return (x_train, y_train), (x_test, y_test)
+
 def prepare_mnist():
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
     x_train = x_train.astype(np.float32) / 255.0
